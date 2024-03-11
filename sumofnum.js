136cba4143fc1902b61c289cd6c0ae985d0a5e40
poojapalkandwar22 <161710505+poojapalkandwar22@users.noreply.github.com>
2024-03-11 11:34:50 +0530

    Add files via upload

---
+++ sumofnum.js
@@ -0,0 +1,82 @@
+// let a=5;
+// let b=20;
+// let c=a+b;
+// console.log(c);
+
+//Write a program to print the sum of num upto 1 to n;
+/*const P= require('readline-sync').questionInt("Enter the num");
+let sum=0;
+for(let i=1;i<=P;i++)
+{
+    sum+=i;
+}
+console.log(sum);*/
+
+/*function example() {
+    var x = 10;
+    if (true) {
+        var x = 20; // This will overwrite the previous value of x
+        console.log(x); // Outputs 20
+    }
+    console.log(x); // Outputs 20
+}
+example();*/
+
+/*function example() {
+    let x = 10;
+    if (true) {
+        let x = 20; // This will overwrite the previous value of x
+        console.log(x); // Outputs 20
+    }
+    console.log(x); // Outputs 20
+}
+example();*/
+
+
+/*const input= require("readline-sync");
+
+let a = input.question("enter a number: "), 
+b = input.question("enter b number: "),
+c= input.question("enter c number: ");
+
+if (a>b){
+    max= a;
+}
+else {
+    max= b;
+}
+if (max > c){
+    console.log("max:",max);
+}
+else{
+    console.log("max:",c);
+}*/
+
+/*let a= null;
+let b= 345;
+let c= "pooja";
+let d= BigInt("567")+ BigInt("33");
+let e;
+let f = Symbol("pooja is my name");
+let g= true;
+console.log(a,b,c,d,e,f,g)*/
+
+// const input= require("readline-sync")
+// let n = input.questionInt("ente n");
+// c=0;
+// for(let i=1;i<=n;i++)
+// {
+//      if(n%i==0)
+//      {
+//      	c++;
+//      }
+// }
+// if(c==2)
+// {
+// 	console.log("prime");
+// }
+// else
+// {
+// 	console.log("Not Prime");
+// }
+
